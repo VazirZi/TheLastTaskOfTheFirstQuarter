68ca9501dcfeffaf396706eff51296217234c271
VazirZi <vazir.zivar@mail.com>
2023-02-16 14:56:38 +0500

    Добавлен файл программы

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Введите длину массива: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            int count = 0;  // счетчик количества элементов для результирующего массива
+
+            string[] array = new string[index];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите {i} элемент массива: ");
+                array[i] = Console.ReadLine();
+                
+                if (array[i].Length <= 3)
+                    count++;
+            }
+
+            string[] resultArray = new string[count];
+            int j = 0;  // индекс элементов результирующего массива
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    resultArray[j] = array[i];
+                    Console.Write($"{resultArray[j]} ");
+                    j++;
+                }
+            }
+        }
+    }
+}
